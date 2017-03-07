ef7a845759450f9fcb6577aa34df4cdc2f7e016a
aorlandi <alessandro.orlandi@buy4now.com>
2017-03-07 18:33:28 +0000

    [Enhancement] - Working on tags

---
+++ WebAPI/src/myVegAppDbAPI/Controllers/Api/TagsApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using MongoDB.Driver;
+using myVegAppDbAPI.Model.Settings;
+using MongoDB.Driver.GridFS;
+using MongoDB.Bson.IO;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace myVegAppDbAPI.Controllers.Api
+{
+    [Route("api/tags")]
+    [EnableCors("MyPolicy")]
+    public class TagsApiController : Controller
+    {
+        private MongoClient _client;
+        private IMongoDatabase _database;
+        private MongoSettings _MongoSettings;
+        private readonly JsonWriterSettings jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+
+        public TagsApiController(IOptions<MongoSettings> mongoSettings, IOptions<EmailSettings> emailSettings)
+        {
+            _MongoSettings = mongoSettings.Value;
+            _client = new MongoClient(_MongoSettings.MongoDbHost);
+            _database = _client.GetDatabase(_MongoSettings.DatabaseName);
+        }
+
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            var placesCollection = _database.GetCollection<BsonDocument>("places");
+            var builder = Builders<BsonDocument>.Filter;
+            return new List<String>();
+        }
+    }
+}
