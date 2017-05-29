92eb0add4c6bbc4fe7cef9b37921b85268bcbf30
= <=>
2017-05-29 18:02:10 +0100

    [Enhancement] - home

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export class Tag{
+  _id: string;
+  isSelected: boolean
+}
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tags: Tag[];
+   maxDistance = 5;
 
+  constructor(public navCtrl: NavController) {
+    this.tags =  [{_id:'#Tag1',isSelected:false},{_id:'#Tag2',isSelected:false},{_id:'#Tag3',isSelected:false}];
   }
 
 }
