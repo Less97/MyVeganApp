517fa5926419ade606de6eadc66bb0c5eb3e67cf
Alessandro Orlandi <Alessandro83Lignano@gmail.com>
2017-06-01 22:50:10 +0100

    [Enhancement] - Json parse

--- myApp/src/entities/userData.ts
+++ myApp/src/entities/userData.ts
@@ -1,13 +1,15 @@
+export class UserData {
+  isLoggedIn: boolean;
+  firstName: string;
+  lastName: string;
+  email: string;
+  id: string;
 
 
-export class UserData{
-  isLoggedIn:boolean;
-  user: {
-    firstName:string;
-    lastName:string;
-    email:string;
-  };
-  _id:{
-    oid:string;
+  public FromJson(json: string): UserData {
+    var obj = Object.create(UserData);
+    var data = JSON.parse(json);
+
+    return obj;
   }
 }
