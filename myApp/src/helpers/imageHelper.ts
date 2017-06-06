ce1b8f307f539da0a3eafe6adfaf1c958e0d95bc
= <=>
2017-06-06 16:04:20 +0100

    [Refactoring] - Moved images

--- myApp/src/helpers/imageHelper.ts
+++ myApp/src/helpers/imageHelper.ts
@@ -9,15 +9,15 @@ export class ImageHelper{
     public static GetImageListSrc(type:string):string{
          switch(type){
           case "restaurant":
-             return "assets/placeTypes/list/restaurant.png";
+             return "assets/images/placeTypes/list/restaurant.png";
           case "cafe":
-             return "assets/placeTypes/list/cafe.png";
+             return "assets/images/placeTypes/list/cafe.png";
           case "shop":
-             return "assets/placeTypes/list/shop.png";
+             return "assets/images/placeTypes/list/shop.png";
           case "food truck":
-            return "assets/placeTypes/list/foodtruck.png";
+            return "assets/images/placeTypes/list/foodtruck.png";
           case "takeaway":
-             return "assets/placeTypes/list/takeaway.png"
+             return "assets/images/placeTypes/list/takeaway.png"
           case "market":
             return ""
         }
@@ -26,15 +26,15 @@ export class ImageHelper{
     public static GetImageMapSrc(type:string):string{
          switch(type){
           case "restaurant":
-            return "assets/placeTypes/pins/restaurant.png";
+            return "assets/images/placeTypes/pins/restaurant.png";
           case "cafe":
-            return "assets/placeTypes/pins/cafe.png";
+            return "assets/images/placeTypes/pins/cafe.png";
           case "shop":
-            return "assets/placeTypes/pins/shop.png";
+            return "assets/images/placeTypes/pins/shop.png";
           case "food truck":
-            return "assets/placeTypes/pins/foodtruck.png";
+            return "assets/images/placeTypes/pins/foodtruck.png";
           case "takeaway":
-            return "assets/placeTypes/pins/takeaway.png"
+            return "assets/images/placeTypes/pins/takeaway.png"
           case "market":
             return ""
         }
