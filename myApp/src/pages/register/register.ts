8f21fdb2db5f3dc1ff0b65bf02dcd1bcaeebda30
Alessandro Orlandi <Alessandro83Lignano@gmail.com>
2017-06-13 22:33:41 +0100

    [Enhancement] - Validation

--- myApp/src/pages/register/register.ts
+++ myApp/src/pages/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { UserService } from '../../services/userService';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html'
@@ -14,7 +14,8 @@ export class RegisterPage {
     
   }
 
-  register(){
+  register(user:any){
+    console.dir(user);
     this.userService.register(this.user.firstName, this.user.lastName, this.user.email, this.user.password);
   }
 }
