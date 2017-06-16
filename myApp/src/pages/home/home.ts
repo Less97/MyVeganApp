2e7d5d0910ecff5a78e5b2a31e953ced6867cc97
= <=>
2017-06-16 17:51:29 +0100

    [Working] - Working on array binding

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -13,22 +13,23 @@ import { ConfigsProvider } from '../../providers/configsProvider'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage {
-
-  searchSettings:SearchSettings;
-  settingObserver:Observable<SearchSettings>;
+  searchSettings:SearchSettings;  
   constructor(public navCtrl: NavController,public tagsService:TagsService,private ga: GoogleAnalytics,private configsProvider:ConfigsProvider) {
-    this.searchSettings = this.configsProvider.getSearchSettings();
+  this.searchSettings = new SearchSettings(30,[]);
+   this.tagsService.getTags().subscribe(ts=>{
+        this.searchSettings.tags = ts;
+        alert('got tags')
+      })
+  }
 
-    tagsService.getTags().subscribe(tags=>{
-      this.searchSettings.tags = tags;
-    })
-    this.settingObserver = Observable.of(this.searchSettings);
+  notifyChange(){
+    this.configsProvider.saveSearchSettings(this.searchSettings);
   }
 
   ionViewDidLoad(){
+      
     this.ga.startTrackerWithId('UA-82832670-5')
    .then(() => {
      
@@ -37,12 +38,6 @@ export class HomePage {
      // You can now track pages or set additional information such as AppVersion or UserId
    })
    .catch(e => console.log('Error starting GoogleAnalytics', e));
-
-    this.settingObserver.subscribe(set=>{
-      alert('changed')
-    });
-    alert('subscribed!');
-    this.searchSettings.maxDistance =250;
   }
 
   goNearby(){
